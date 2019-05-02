52dfcff79ad62c27f067a588dd861f24c872d44f
Will Fritts <waf515@gmail.com>
2019-05-02 09:16:45 -0700

    created server, updated readme

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
 
 const app = express();
 
